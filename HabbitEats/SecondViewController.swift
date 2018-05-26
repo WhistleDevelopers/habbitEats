7c9e5e7b1b32bd5c07e80d777fe9156331931b2b
whistledevelopers <whistledevelopers@gmail.com>
2018-05-26 19:57:19 +0800

    Add .gitignore file

---
+++ HabbitEats/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  HabbitEats
+//
+//  Created by Guest User on 26/05/18.
+//  Copyright © 2018 WhistleDevelopers. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
